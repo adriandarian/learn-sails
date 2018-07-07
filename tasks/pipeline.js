edd9e3d0bd4fa72c008c65bf6a69e7cf91d51bce
Adrian Darian <adarian@ucmerced.edu>
2018-07-07 10:45:14 -0700

    bower update

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -64,6 +64,11 @@ var jsFilesToInject = [
   // Load `sails.io` before everything else.
   'dependencies/sails.io.js',
 
+  // Vendor dependencies
+  'vendor/jquery/dist/jquery.js',
+  'vendor/semantic/dist/semantic.js',
+  'vendor/jsrender/jsrender.js',
+
   // Bring in `.js` files for any other client-side JavaScript dependencies.
   // (e.g. Lodash, Vue.js, jQuery, Bootstrap, Ember, Angular, etc.)
   // > Be sure to list dependencies that depend on each other in the right order!
